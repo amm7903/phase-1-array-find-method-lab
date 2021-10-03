7d7f465e340675f912893b512c1f43e5729337db
Alejandro Marcano <+>
2021-10-02 23:19:50 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+// superbowlWin = (record) => {
+//     const result = record.find(record => record.result === 'W');
+//     return !! result ? result.year : undefined;
+
+// }
+
+// function superbowlWin(record){
+
+// }
+
+let superbowlWin = function (record) {
+    const result = record.find(function (record) {
+        return record.result === 'W'
+    })
+    return result ? result.year : undefined;
+}
+
 
